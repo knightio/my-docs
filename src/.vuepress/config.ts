c439851be29c26a88c2fe75020ae2f1b591f6c7e
knightio <55305535+knightio@users.noreply.github.com>
2024-12-27 17:22:45 +0800

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/my-docs",
 
   lang: "zh-CN",
   title: "一切为了更好的自己",
